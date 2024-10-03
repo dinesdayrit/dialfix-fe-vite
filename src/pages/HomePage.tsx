29be01864ba4dbaef58488b857fcbee3f2b35b3b
Dines Dayrit <84615498+dinesdayrit@users.noreply.github.com>
2024-10-03 16:02:31 +0800

    feat: add brands section on homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,11 @@
+import Brands from "@/components/landingPage/Brands";
 import Hero from "@/components/landingPage/Hero";
 
 export default function HomePage() {
-  return <Hero />;
+  return (
+    <>
+      <Hero />
+      <Brands />
+    </>
+  );
 }
