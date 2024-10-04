85d447cb11fe9231ee8790cf5ba7f07dcfa1baa0
Dines Dayrit <84615498+dinesdayrit@users.noreply.github.com>
2024-10-04 16:17:55 +0800

    feat: add loading on getting services

--- src/pages/ManageServicesPage.tsx
+++ src/pages/ManageServicesPage.tsx
@@ -1,10 +1,15 @@
 import { useCreateMyServices, useGetMyServices } from "@/api/MyServicesApi";
+import Spinner from "@/components/Spinner";
 import ManageServicesForm from "@/forms/manage-services-form/ManageServicesForm";
 
 export default function ManageServicesPage() {
   const mutation = useCreateMyServices();
   const { mutateAsync: createService } = mutation;
-  const { services } = useGetMyServices();
+  const { services, isLoading: isGetLoading } = useGetMyServices();
+
+  if (isGetLoading) {
+    return <Spinner text="Getting Services.." />;
+  }
 
   return (
     <div>
