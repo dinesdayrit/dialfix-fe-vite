6a748fbe5b53437cdb11eb9567f44985253d6b7a
Dines Dayrit <84615498+dinesdayrit@users.noreply.github.com>
2024-10-25 12:24:54 +0800

    update layout

--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -10,7 +10,7 @@ const Layout = ({ children }: Props) => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <div className="flex-1">{children}</div>
+      <div className="container mx-auto flex-1">{children}</div>
       <Footer />
     </div>
   );
