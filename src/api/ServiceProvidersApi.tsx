6c8723c3029e29c07d9f8762c2f9f90352a2c06e
Dines Dayrit <84615498+dinesdayrit@users.noreply.github.com>
2024-10-30 11:35:42 +0800

    feat: add useGetProvider fetch hook

--- src/api/ServiceProvidersApi.tsx
+++ src/api/ServiceProvidersApi.tsx
@@ -1,33 +1,29 @@
 import { SearchState } from "@/pages/SearchPage";
-import { ProvidersSearchResponse } from "@/types";
+import { ProvidersSearchResponse, Services } from "@/types";
 import { useQuery } from "@tanstack/react-query";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
-export const useFetchAllServiceProviders = () => {
-  const fetchAllServiceProviders = async () => {
-    const response = await fetch(`${API_BASE_URL}/api/serviceProviders`);
+export const useGetProvider = (serviceProviderId?: string) => {
+  const getProviderByIdRequest = async (): Promise<Services> => {
+    const response = await fetch(
+      `${API_BASE_URL}/api/serviceProviders/${serviceProviderId}`
+    );
 
     if (!response.ok) {
-      throw new Error("Failed to fetch service providers");
+      throw new Error("Failed to get Provider");
     }
 
     return response.json();
   };
-  const {
-    data: serviceProviders,
-    isLoading,
-    error,
-  } = useQuery({
-    queryKey: ["fetchCurrentUser"],
-    queryFn: fetchAllServiceProviders,
+
+  const { data: serviceProvider, isLoading } = useQuery({
+    queryKey: ["fetchProvider"],
+    queryFn: getProviderByIdRequest,
+    enabled: !!serviceProviderId,
   });
 
-  return {
-    serviceProviders,
-    isLoading,
-    error,
-  };
+  return { serviceProvider, isLoading };
 };
 
 export const useSearchProviders = (searchState: SearchState, city?: string) => {
