3c2a315d5ba1f6607659543b353515f1de6fbd0e
Dines Dayrit <84615498+dinesdayrit@users.noreply.github.com>
2024-10-31 15:12:07 +0800

    update login button

--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -2,6 +2,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 import { Button } from "./ui/button";
 import UsernameMenu from "./UsernameMenu";
 import { Link } from "react-router-dom";
+import { LogInIcon } from "lucide-react";
 
 export default function MainNav() {
   const { loginWithRedirect, isAuthenticated } = useAuth0();
@@ -9,18 +10,17 @@ export default function MainNav() {
     <span className="flex space-x-2 items-center">
       {isAuthenticated ? (
         <>
-          <Link to="/order-status" className="font-bold hover:text-orange-500">
+          <Link to="/" className="font-bold hover:text-green-500">
             Order Status
           </Link>
           <UsernameMenu />
         </>
       ) : (
         <Button
-          variant="ghost"
-          className="font-bold hover:bg-transparent text-zinc-600"
+          className="text-sm font-semibold leading-6 py-4 px-4 rounded-md"
           onClick={async () => await loginWithRedirect()}
         >
-          Sign in
+          <LogInIcon /> Log in
         </Button>
       )}
     </span>
