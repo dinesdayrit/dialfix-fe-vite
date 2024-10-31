e41ab79ca648877d605f1a5df3baf9be0794799a
Dines Dayrit <84615498+dinesdayrit@users.noreply.github.com>
2024-10-31 16:30:01 +0800

    add animation on header when scrolled down

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,14 +2,32 @@ import { Link } from "react-router-dom";
 import { Wrench } from "lucide-react";
 import MobileNav from "./MobileNav";
 import MainNav from "./MainNav";
+import { useEffect, useState } from "react";
 
 export default function Header() {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrolled(window.scrollY > 0);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <div className="fixed flex items-center justify-between py-4 bg-white bg-opacity-90 w-screen z-30">
+    <div
+      className={`fixed flex items-center justify-between py-4 w-screen z-30 transition-colors duration-500 ${
+        scrolled ? "bg-slate-100 bg-opacity-90" : "bg-white bg-opacity-90"
+      }`}
+    >
       <div className="container flex justify-between items-center">
         <Link
           to="/"
-          className="text-3xl font-extrabold tracking-tight text-orange-500 flex"
+          className={`flex items-center font-extrabold tracking-tight transition-all duration-500 ${
+            scrolled ? "text-2xl" : "text-3xl"
+          } text-orange-500`}
         >
           dial
           <span className="text-green-600 flex justify-center items-center">
