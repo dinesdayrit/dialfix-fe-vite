4a87197b1e66d60b435a55f2fd62dc0ac0b1c24a
Dines Dayrit <84615498+dinesdayrit@users.noreply.github.com>
2024-10-31 14:36:58 +0800

    update detailsPage

--- src/pages/DetailsPage.tsx
+++ src/pages/DetailsPage.tsx
@@ -28,7 +28,8 @@ export default function DetailsPage() {
 
       <div className="flex flex-col">
         <h1 className="text-3xl">{serviceProvider.serviceProviderName}</h1>
-        <p>
+        <p className="text-slate-500">
+          {serviceProvider.addressLine1} <br />
           {serviceProvider.city}, {serviceProvider.country}
         </p>
 
