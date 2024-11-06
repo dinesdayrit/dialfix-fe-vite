4b65784061604ac441d2e704d3fc5ff31289b872
Dines Dayrit <84615498+dinesdayrit@users.noreply.github.com>
2024-11-06 15:15:50 +0800

    refactor useGetLocation custom hook

--- src/api/GetLocation.tsx
+++ src/api/GetLocation.tsx
@@ -1,15 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
 
-async function fetchLocation() {
-  const response = await fetch(`https://ipinfo.io/json`);
-  if (!response.ok) {
-    throw new Error("Error fetching location data");
-  }
-  const data = await response.json();
-  return data.city || "City not found";
-}
-
 export default function useGetLocation() {
+  async function fetchLocation() {
+    const response = await fetch(`https://ipinfo.io/json`);
+    if (!response.ok) {
+      throw new Error("Error fetching location data");
+    }
+    const data = await response.json();
+    return data.city || "City not found";
+  }
+
   return useQuery({
     queryKey: ["location"],
     queryFn: fetchLocation,
