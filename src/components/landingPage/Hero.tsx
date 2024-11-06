599dd234c1880a109e975cb8f4f9896d4a47ba8e
Dines Dayrit <84615498+dinesdayrit@users.noreply.github.com>
2024-11-06 15:47:58 +0800

    update hero section

--- src/components/landingPage/Hero.tsx
+++ src/components/landingPage/Hero.tsx
@@ -1,16 +1,11 @@
-import SearchBar, { SearchForm } from "@/components/SearchBar";
+import useGetLocation from "@/api/GetLocation";
 import TransitionalText from "@/components/landingPage/TransitionalText";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { Button } from "../ui/button";
 
 const Hero = () => {
   const TEXTS = ["PC-Tech", "Plumber", "Electrician", "Mechanic"];
-  const navigate = useNavigate();
-
-  const handleSubmit = (searchFormValues: SearchForm) => {
-    navigate({
-      pathname: `/search-service-provider/${searchFormValues.searchQuery}`,
-    });
-  };
+  const { data } = useGetLocation();
 
   return (
     <div className="bg-gradient-to-b from-cyan-300 to-green-300 ">
@@ -31,57 +26,51 @@ const Hero = () => {
                 <p className="mb-8 max-w-[480px] text-gray-800 text-body-color ">
                   Find the right fix, fast. Your trusted pros, all in one app.
                 </p>
-                <SearchBar
-                  placeHolder="Search by City or Town"
-                  onSubmit={handleSubmit}
-                />
-                <ul className="flex flex-wrap items-center mt-6">
-                  <li>
-                    <a
-                      href="/#"
-                      className="inline-flex items-center justify-center rounded-md bg-blue-600 px-6 py-3 text-center text-base font-medium text-white hover:bg-blue-700 lg:px-7"
-                    >
-                      Get started
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="/#"
-                      className="inline-flex items-center justify-center px-5 py-3 text-center text-base font-medium text-[#464646] hover:text-primary dark:text-white"
-                    >
-                      <span className="mr-2">
-                        <svg
-                          width="24"
-                          height="25"
-                          viewBox="0 0 24 25"
-                          fill="none"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <circle cx="12" cy="12.6152" r="12" fill="#3758F9" />
-                          <rect
-                            x="7.99893"
-                            y="14.979"
-                            width="8.18182"
-                            height="1.63636"
-                            fill="white"
-                          />
-                          <rect
-                            x="11.2717"
-                            y="7.61523"
-                            width="1.63636"
-                            height="4.09091"
-                            fill="white"
-                          />
-                          <path
-                            d="M12.0898 14.1606L14.9241 11.0925H9.25557L12.0898 14.1606Z"
-                            fill="white"
-                          />
-                        </svg>
-                      </span>
-                      Download App
-                    </a>
-                  </li>
-                </ul>
+                <div className="flex flex-col items-start">
+                  <Link
+                    to={`/search-service-provider/${data}`}
+                    className="inline-flex items-center justify-center border p-4 rounded-full border-blue-500 md:text-xl bg-blue-600 text-white"
+                  >
+                    Available Service Providers on your location
+                  </Link>
+
+                  <a
+                    href="/#"
+                    className="inline-flex items-center justify-center px-5 py-3 text-center text-base font-medium text-[#464646] hover:text-primary dark:text-white"
+                  >
+                    <span className="mr-2">
+                      <svg
+                        width="24"
+                        height="25"
+                        viewBox="0 0 24 25"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <circle cx="12" cy="12.6152" r="12" fill="#3758F9" />
+                        <rect
+                          x="7.99893"
+                          y="14.979"
+                          width="8.18182"
+                          height="1.63636"
+                          fill="white"
+                        />
+                        <rect
+                          x="11.2717"
+                          y="7.61523"
+                          width="1.63636"
+                          height="4.09091"
+                          fill="white"
+                        />
+                        <path
+                          d="M12.0898 14.1606L14.9241 11.0925H9.25557L12.0898 14.1606Z"
+                          fill="white"
+                        />
+                      </svg>
+                    </span>
+                    Download App
+                  </a>
+                </div>
+
                 <div className="py-4 flex pt-8 gap-8">
                   <div className="flex flex-col items-center justify-center">
                     <span className="font-bold text-slate-900">300</span>
