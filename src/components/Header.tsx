1f99512bd7d4b762806180c137ca9bd44d876a18
Dines Dayrit <84615498+dinesdayrit@users.noreply.github.com>
2024-11-10 13:23:10 +0800

    adjust header bg-color:

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,7 +18,7 @@ export default function Header() {
 
   return (
     <div className="fixed flex flex-col transition-colors duration-500 shadow-md z-30">
-      <div className="bg-green-600 w-screen flex items-center justify-center">
+      <div className="bg-green-500 w-screen flex items-center justify-center">
         <p className="text-white">
           SUBSCRIBE TO OUR NEWS LETTER FOR DISCOUNTS AND SPECIAL OFFERS!
         </p>
