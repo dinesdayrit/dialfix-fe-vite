e976f5e2aecb9ee67a282cc54d569914f775d9eb
Dines Dayrit <84615498+dinesdayrit@users.noreply.github.com>
2024-11-13 15:46:22 +0800

    update ServicesDetails on detailsPage

--- src/components/serviceProvidersDetailsPage/ServicesDetails.tsx
+++ src/components/serviceProvidersDetailsPage/ServicesDetails.tsx
@@ -12,14 +12,32 @@ export default function ServicesDetails() {
 
   return (
     <div>
-      {serviceProvider.serviceItems.map((item: ServiceItem) => (
-        <div key={item._id} className="p-2">
-          <h3 className="font-bold flex">
-            <Dot />
-            {item.name}
-          </h3>
-        </div>
-      ))}
+      <div className="border-b-2 p-2 mb-8">
+        <p className="font-bold mb-2">About</p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+      </div>
+
+      <div className="border-b-2 mb-8">
+        <p className="font-bold mb-2">Services Offers</p>
+        {serviceProvider.serviceItems.map((item: ServiceItem) => (
+          <div key={item._id} className="p-2">
+            <h3 className="font-bold flex">
+              <Dot />
+              {item.name}
+            </h3>
+          </div>
+        ))}
+      </div>
+
+      <div className="border-b-2 mb-8 p-2">
+        <p className="font-bold mb-2">Office Hours</p>
+        <p>Monday - Friday: 9:00 AM - 6:00 PM</p>
+        <p>Saturday: 10:00 AM - 4:00 PM</p>
+        <p>Sunday: Closed</p>
+      </div>
     </div>
   );
 }
