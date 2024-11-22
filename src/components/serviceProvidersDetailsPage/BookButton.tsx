06e53a188c177313d9c2b833111bc25033691615
Dines Dayrit <84615498+dinesdayrit@users.noreply.github.com>
2024-11-22 14:35:31 +0800

    feat: check if user is login before proceeding to book

--- src/components/serviceProvidersDetailsPage/BookButton.tsx
+++ src/components/serviceProvidersDetailsPage/BookButton.tsx
@@ -1,7 +1,38 @@
 import { ArrowRight } from "lucide-react";
 import { Button } from "../ui/button";
+import { useAuth0 } from "@auth0/auth0-react";
+import { useLocation } from "react-router-dom";
+import Spinner from "../Spinner";
 
 export default function BookButton() {
+  const {
+    isAuthenticated,
+    isLoading: isAuthLoading,
+    loginWithRedirect,
+  } = useAuth0();
+
+  const { pathname } = useLocation();
+
+  const onLogin = async () => {
+    await loginWithRedirect({
+      appState: {
+        returnTo: pathname,
+      },
+    });
+  };
+
+  if (!isAuthenticated) {
+    return (
+      <Button onClick={onLogin} className="bg-orange-500 flex-1">
+        Log in to Book
+      </Button>
+    );
+  }
+
+  if (isAuthLoading) {
+    return <Spinner />;
+  }
+
   return (
     <div>
       <Button className="bg-orange-600 hover:bg-orange-600/90 py-6">
