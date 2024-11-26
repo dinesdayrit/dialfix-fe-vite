7ed3d78bb2c63eb811aa01eb3212514653d525b8
Dines Dayrit <84615498+dinesdayrit@users.noreply.github.com>
2024-11-26 21:09:27 +0800

    style: update my booked appointments page

--- src/pages/MyAppointmentsPage.tsx
+++ src/pages/MyAppointmentsPage.tsx
@@ -13,29 +13,31 @@ export default function MyAppointmentsPage() {
   }
 
   return (
-    <div className="container p-4">
-      <h1 className="text-2xl font-bold mb-4">My Appointments</h1>
-      {appointments.map((appointment) => (
-        <div key={appointment._id} className="border mb-10 p-6">
-          <p>
-            <strong>Providers Name:</strong>{" "}
-            {appointment.ServiceProvider.serviceProviderName}
-          </p>
-          <p>
-            <strong>Providers Address:</strong>{" "}
-            {appointment.ServiceProvider.addressLine1}{" "}
-            {appointment.ServiceProvider.city}
-          </p>
-          <p>
-            <strong>Appointment Date:</strong>{" "}
-            {appointment.apointmentDetails.appointmentDate}
-          </p>
-          <p>
-            <strong>Appointment Status:</strong> {appointment.status}{" "}
-            {appointment.status === "placed" && "(waiting for confirmation)"}
-          </p>
-        </div>
-      ))}
+    <div className="container">
+      <h1 className="text-2xl font-bold mb-4 mt-10">My Appointments</h1>
+      <ul className="list-disc pl-5 space-y-6 mt-6">
+        {appointments.map((appointment) => (
+          <li key={appointment._id} className="border-t-2 pt-4">
+            <p className="mb-2">
+              <strong>Provider's Name:</strong>{" "}
+              {appointment.ServiceProvider.serviceProviderName}
+            </p>
+            <p className="mb-2">
+              <strong>Provider's Address:</strong>{" "}
+              {appointment.ServiceProvider.addressLine1},{" "}
+              {appointment.ServiceProvider.city}
+            </p>
+            <p className="mb-2">
+              <strong>Appointment Date:</strong>{" "}
+              {appointment.apointmentDetails.appointmentDate}
+            </p>
+            <p className="uppercase text-lg">
+              <strong>Appointment Status:</strong> {appointment.status}{" "}
+              {appointment.status === "placed" && "(waiting for confirmation)"}
+            </p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
