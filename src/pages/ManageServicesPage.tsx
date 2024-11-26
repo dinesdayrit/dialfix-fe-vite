f09ebfce58bb2f9dcc7bd431a8e4f6368d966ea7
Dines Dayrit <84615498+dinesdayrit@users.noreply.github.com>
2024-11-26 21:23:58 +0800

    use tabs component from shadcn on manage your services page

--- src/pages/ManageServicesPage.tsx
+++ src/pages/ManageServicesPage.tsx
@@ -4,7 +4,9 @@ import {
   useUpdateMyServices,
 } from "@/api/MyServicesApi";
 import Spinner from "@/components/Spinner";
+import { Tabs } from "@/components/ui/tabs";
 import ManageServicesForm from "@/forms/manage-services-form/ManageServicesForm";
+import { TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { useEffect } from "react";
 
 export default function ManageServicesPage() {
@@ -34,12 +36,25 @@ export default function ManageServicesPage() {
   }
 
   return (
-    <div className="container pt-10">
-      <ManageServicesForm
-        services={services}
-        onSave={isEditing ? updateService : createService}
-        isLoading={isCreateLoading || isUpdateLoading}
-      />
-    </div>
+    <Tabs className="container mx-auto mt-10">
+      <TabsList>
+        <TabsTrigger value="appointments">Appointments</TabsTrigger>
+        <TabsTrigger value="manage-services">Manage Shop</TabsTrigger>
+      </TabsList>
+      <TabsContent value="manage-services">
+        <ManageServicesForm
+          services={services}
+          onSave={isEditing ? updateService : createService}
+          isLoading={isCreateLoading || isUpdateLoading}
+        />
+      </TabsContent>
+
+      <TabsContent
+        value="appointments"
+        className="space-y-5 bg-gray-50 pg-10 rounded-lg"
+      >
+        <div>appointments</div>
+      </TabsContent>
+    </Tabs>
   );
 }
