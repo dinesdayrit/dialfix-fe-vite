3e390390675745d68b8f184edb02c898b7df49c7
Dines Dayrit <84615498+dinesdayrit@users.noreply.github.com>
2024-11-26 21:55:33 +0800

    update MyAppointments Page

--- src/pages/MyAppointmentsPage.tsx
+++ src/pages/MyAppointmentsPage.tsx
@@ -1,5 +1,6 @@
 import { useGetMyAppointments } from "@/api/AppointmentApi";
 import Spinner from "@/components/Spinner";
+import { Separator } from "@/components/ui/separator";
 
 export default function MyAppointmentsPage() {
   const { appointments, isLoading } = useGetMyAppointments();
@@ -13,26 +14,27 @@ export default function MyAppointmentsPage() {
   }
 
   return (
-    <div className="container">
+    <div className="container p-2">
       <h1 className="text-2xl font-bold mb-4 mt-10">My Appointments</h1>
-      <ul className="list-disc pl-5 space-y-6 mt-6">
-        {appointments.map((appointment) => (
-          <li key={appointment._id} className="border-t-2 pt-4">
-            <p className="mb-2">
+      <ul className="list-disc pl-5 space-y-6 mt-6 border-b-8 pb-10">
+        {appointments.map((appointment, index) => (
+          <li key={index} className="pt-4 ">
+            <Separator className="mb-2" />
+            <p className="mb-2 ">
               <strong>Provider's Name:</strong>{" "}
-              {appointment.ServiceProvider.serviceProviderName}
+              {appointment.ServiceProvider?.serviceProviderName}
             </p>
             <p className="mb-2">
               <strong>Provider's Address:</strong>{" "}
-              {appointment.ServiceProvider.addressLine1},{" "}
-              {appointment.ServiceProvider.city}
+              {appointment.ServiceProvider?.addressLine1},{" "}
+              {appointment.ServiceProvider?.city}
             </p>
             <p className="mb-2">
               <strong>Appointment Date:</strong>{" "}
-              {appointment.apointmentDetails.appointmentDate}
+              {appointment.apointmentDetails?.appointmentDate}
             </p>
             <p className="uppercase text-lg">
-              <strong>Appointment Status:</strong> {appointment.status}{" "}
+              <strong>Appointment Status:</strong> {appointment?.status}{" "}
               {appointment.status === "placed" && "(waiting for confirmation)"}
             </p>
           </li>
