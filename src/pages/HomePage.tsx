8bc13a5fe0abfaf8a8f00e741430182a7d002a0d
Dines Dayrit <84615498+dinesdayrit@users.noreply.github.com>
2024-11-29 20:06:31 +0800

    add loading state to get the location before rendering homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,17 @@
+import useGetLocation from "@/api/GetLocation";
 import About from "@/components/landingPage/About";
 import Brands from "@/components/landingPage/Brands";
 import Faq from "@/components/landingPage/Faq";
 import FeatureProviders from "@/components/landingPage/FeatureProviders";
 import Hero from "@/components/landingPage/Hero";
+import Spinner from "@/components/Spinner";
 
 export default function HomePage() {
+  const { isLoading } = useGetLocation();
+
+  if (isLoading) {
+    return <Spinner text="getting your location" />;
+  }
   return (
     <>
       <Hero />
