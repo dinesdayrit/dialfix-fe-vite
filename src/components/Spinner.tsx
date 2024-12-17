6a2791b5356b20f4c6a3c2e0619c2e81b56181cb
Dines Dayrit <84615498+dinesdayrit@users.noreply.github.com>
2024-12-17 09:53:46 +0800

    update size of spinner

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -5,9 +5,9 @@ type Props = {
 export default function Spinner({ text }: Props) {
   return (
     <div className="flex flex-col items-center justify-center mt-20">
-      <div className="h-8 w-8 animate-spin rounded-full border-4 border-gray-300 border-t-green-500 dark:border-gray-600 dark:border-t-gray-400" />
+      <div className="h-10 w-10 animate-spin rounded-full border-4 border-gray-300 border-t-green-500 dark:border-gray-600 dark:border-t-gray-400" />
       {text && (
-        <p className="text-orange-500 dark:text-gray-400 animate-pulse mt-2">
+        <p className="text-orange-500 dark:text-gray-400 animate-pulse mt-2 text-xl">
           {text}
         </p>
       )}
