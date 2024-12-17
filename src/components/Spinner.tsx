80fbb12ec734cf3bbeacb3637d47d45df10f33ae
Dines Dayrit <84615498+dinesdayrit@users.noreply.github.com>
2024-12-17 17:11:13 +0800

    update spinner

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,11 +1,16 @@
-type Props = {
-  text?: string;
-};
-
-export default function Spinner({ text }: Props) {
+export default function Spinner({ text }: { text?: string }) {
   return (
-    <div className="flex flex-col items-center justify-center mt-20">
-      <div className="h-10 w-10 animate-spin rounded-full border-4 border-gray-300 border-t-green-500 dark:border-gray-600 dark:border-t-gray-400" />
+    <div className="flex flex-col items-center justify-center mt-20 relative">
+      {/* Spinner */}
+      <div className="relative flex items-center justify-center">
+        <div className="h-24 w-24 animate-spin rounded-full border-4 border-gray-300 border-t-green-500 dark:border-gray-600 dark:border-t-gray-400 bg-white" />
+
+        {/* Logo inside the spinner */}
+        <div className="absolute flex items-center justify-center">
+          <img src="/logo.png" alt="logo" className="h-44 object-cover mt-3" />
+        </div>
+      </div>
+
       {text && (
         <p className="text-orange-500 dark:text-gray-400 animate-pulse mt-2 text-xl">
           {text}
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -15,7 +15,7 @@ export default function HomePage() {
       <>
         {isLoading && (
           <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-60 z-50">
-            <Spinner text="Getting your location..." />
+            <Spinner />
           </div>
         )}
         <Hero />
